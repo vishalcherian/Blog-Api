74b7082e5121bd215ce7fa98f36d93ffe51849ab
c98580 <vishal.cherian@cigna.com>
2020-05-08 11:33:17 -0400

    cors enabled, added get patch and delete endpoints for blog posts

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ import posts from './routes/posts'
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, () => console.log("connected to DB"));
 
 // MIDDLEWARE - function that executes when routes are hit
 app.use(bodyParser.json())  //makes sure that you can parse the request body (converts to json)
+app.use(cors());
 
 //ROUTES
-
 app.use(home);
 app.use('/posts', posts);
 
--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,6 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', (req, res) => {
-    console.log(req.body);
     let post = {}
     if(req.body.title) post.title = req.body.title;
     if(req.body.subtitle) post.subtitle = req.body.subtitle;
@@ -28,6 +27,39 @@ router.get('/', (req, res) => {
             res.status(200).json({success: true, posts: postArray})
         }
     });
+})
+
+router.get('/:id', (req, res) => {
+    Post.find({_id: req.params.id}, (err, post) => {
+        if(err) res.status(404).json({success: false, message: err})
+        else {
+            res.status(200).json({success: true, post: post});
+        }
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    Post.deleteOne({_id: req.params.id}, (err) => {
+        if(err) res.status(404).json({success: false, message: err})
+        else res.status(200).json({success: true})
+    })
+})
+
+router.patch('/:id', (req, res) => {
+    let updatedInfo = {}
+    if(req.body.title) updatedInfo.title = req.body.title;
+    if(req.body.subtitle) updatedInfo.subtitle = req.body.subtitle;
+    if(req.body.date) updatedInfo.date = req.body.date;
+    if(req.body.author) updatedInfo.author = req.body.author;
+    if(req.body.tags) updatedInfo.tags = req.body.tags;
+    if(req.body.article) updatedInfo.article = req.body.article;
+
+    Post.update({_id: req.params.id}, {$set: updatedInfo}, (err, post) => {
+        if(err) res.status(404).json({success: false, message: err})
+        else {
+            res.status(200).json({success: true, post: post});
+        }
+    })
 })
 
 export default router;
